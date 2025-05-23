69a7597e99a61301dd3cb0605b96e1bc8fbde91c
Creatr <creatr@getcreatr.com>
2025-05-23 11:53:36 +0000

    Integrate component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,107 +7,116 @@ export default {
 	darkMode: ["class"],
 	content: ["./src/**/*.tsx"],
 	theme: {
-		opacity: {
-			'0': '0',
-			'10': '0.1',
-			'20': '0.2',
-			'30': '0.3',
-			'40': '0.4',
-			'50': '0.5',
-			'60': '0.6',
-			'70': '0.7',
-			'80': '0.8',
-			'90': '0.9',
-			'100': '1',
-		},
-		extend: {
-			fontFamily: {
-				sans: [
-					'var(--font-geist-sans)',
-					...defaultTheme.fontFamily.sans
-				]
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
-			},
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)',
-				card: {
-					DEFAULT: 'var(--card)',
-					foreground: 'var(--card-foreground)'
-				},
-				popover: {
-					DEFAULT: 'var(--popover)',
-					foreground: 'var(--popover-foreground)'
-				},
-				primary: {
-					DEFAULT: 'var(--primary)',
-					foreground: 'var(--primary-foreground)'
-				},
-				secondary: {
-					DEFAULT: 'var(--secondary)',
-					foreground: 'var(--secondary-foreground)'
-				},
-				muted: {
-					DEFAULT: 'var(--muted)',
-					foreground: 'var(--muted-foreground)'
-				},
-				accent: {
-					DEFAULT: 'var(--accent)',
-					foreground: 'var(--accent-foreground)'
-				},
-				destructive: {
-					DEFAULT: 'var(--destructive)',
-					foreground: 'var(--destructive-foreground)'
-				},
-				border: 'var(--border)',
-				input: 'var(--input)',
-				ring: 'var(--ring)',
-				chart: {
-					'1': 'var(--chart-1)',
-					'2': 'var(--chart-2)',
-					'3': 'var(--chart-3)',
-					'4': 'var(--chart-4)',
-					'5': 'var(--chart-5)'
-				},
-				sidebar: {
-					DEFAULT: 'var(--sidebar-background)',
-					foreground: 'var(--sidebar-foreground)',
-					primary: 'var(--sidebar-primary)',
-					'primary-foreground': 'var(--sidebar-primary-foreground)',
-					accent: 'var(--sidebar-accent)',
-					'accent-foreground': 'var(--sidebar-accent-foreground)',
-					border: 'var(--sidebar-border)',
-					ring: 'var(--sidebar-ring)'
-				}
-			},
-			keyframes: {
-				'accordion-down': {
-					from: {
-						height: '0'
-					},
-					to: {
-						height: 'var(--radix-accordion-content-height)'
-					}
-				},
-				'accordion-up': {
-					from: {
-						height: 'var(--radix-accordion-content-height)'
-					},
-					to: {
-						height: '0'
-					}
-				}
-			},
-			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out'
-			}
-		}
-	},
+    	opacity: {
+    		'0': '0',
+    		'10': '0.1',
+    		'20': '0.2',
+    		'30': '0.3',
+    		'40': '0.4',
+    		'50': '0.5',
+    		'60': '0.6',
+    		'70': '0.7',
+    		'80': '0.8',
+    		'90': '0.9',
+    		'100': '1'
+    	},
+    	extend: {
+    		fontFamily: {
+    			sans: [
+    				'var(--font-geist-sans)',
+                    ...defaultTheme.fontFamily.sans
+                ]
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {
+    			background: 'var(--background)',
+    			foreground: 'var(--foreground)',
+    			card: {
+    				DEFAULT: 'var(--card)',
+    				foreground: 'var(--card-foreground)'
+    			},
+    			popover: {
+    				DEFAULT: 'var(--popover)',
+    				foreground: 'var(--popover-foreground)'
+    			},
+    			primary: {
+    				DEFAULT: 'var(--primary)',
+    				foreground: 'var(--primary-foreground)'
+    			},
+    			secondary: {
+    				DEFAULT: 'var(--secondary)',
+    				foreground: 'var(--secondary-foreground)'
+    			},
+    			muted: {
+    				DEFAULT: 'var(--muted)',
+    				foreground: 'var(--muted-foreground)'
+    			},
+    			accent: {
+    				DEFAULT: 'var(--accent)',
+    				foreground: 'var(--accent-foreground)'
+    			},
+    			destructive: {
+    				DEFAULT: 'var(--destructive)',
+    				foreground: 'var(--destructive-foreground)'
+    			},
+    			border: 'var(--border)',
+    			input: 'var(--input)',
+    			ring: 'var(--ring)',
+    			chart: {
+    				'1': 'var(--chart-1)',
+    				'2': 'var(--chart-2)',
+    				'3': 'var(--chart-3)',
+    				'4': 'var(--chart-4)',
+    				'5': 'var(--chart-5)'
+    			},
+    			sidebar: {
+    				DEFAULT: 'var(--sidebar-background)',
+    				foreground: 'var(--sidebar-foreground)',
+    				primary: 'var(--sidebar-primary)',
+    				'primary-foreground': 'var(--sidebar-primary-foreground)',
+    				accent: 'var(--sidebar-accent)',
+    				'accent-foreground': 'var(--sidebar-accent-foreground)',
+    				border: 'var(--sidebar-border)',
+    				ring: 'var(--sidebar-ring)'
+    			}
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			},
+    			aurora: {
+    				from: {
+    					backgroundPosition: '50% 50%, 50% 50%'
+    				},
+    				to: {
+    					backgroundPosition: '350% 50%, 350% 50%'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			aurora: 'aurora 60s linear infinite'
+    		}
+    	}
+    },
 	plugins: [animate, addVariablesForColors],
 } satisfies Config;
 
